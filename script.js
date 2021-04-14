6195806f4d8e7ed80c07159791bbe705bda61541
Catambimbas <endermenzaogamer@gmail.com>
2021-04-14 18:26:13 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Elements HTML
+let body = document.querySelector('body')
+// Buttons
+let button = document.querySelector('input#but_p2')
+button.addEventListener('click', buttonF)
+function buttonF() {
+    let FO = document.querySelector('input#FO')
+    let DE = document.querySelector('input#DE')
+    let CO = document.querySelector('input#CO')
+    let IN = document.querySelector('input#IN')
+    let SA = document.querySelector('input#SA')
+    let CA = document.querySelector('input#CA')
+    FO = Number(FO.value)
+    DE = Number(DE.value)
+    CO = Number(CO.value)
+    IN = Number(IN.value)
+    SA = Number(SA.value)
+    CA = Number(CA.value)
+    let lista = [FO, DE, CO, IN, SA, CA]
+    for (let c = 0; c < 6; c++){
+        if (isNaN(lista[c]) == true) {
+            alert('Digite apenas números')
+            break
+        } else {
+            alert('Normal')
+            if (c == 5) {
+                apr()
+            }
+        }
+    }
+    function apr(){
+        body.innerHTML = ``
+    }
+}
