fb5e68db10f827e0036b79a4c93ecbd8c498e7a5
Catambimbas <endermenzaogamer@gmail.com>
2021-04-16 10:36:14 -0300

    VIII commit

--- script.js
+++ script.js
@@ -10,25 +10,31 @@ function buttonF() {
     let IN = document.querySelector('input#IN')
     let SA = document.querySelector('input#SA')
     let CA = document.querySelector('input#CA')
-    FO = Number(FO.value)
-    DE = Number(DE.value)
-    CO = Number(CO.value)
-    IN = Number(IN.value)
-    SA = Number(SA.value)
-    CA = Number(CA.value)
-    let lista = [FO, DE, CO, IN, SA, CA]
-    for (let c = 0; c < 6; c++){
-        if (isNaN(lista[c]) == true) {
-            console.log('Numero')
-            break
-        } else {
-            console.log('Normal')
-            if (c == 5) {
-                apr()
+    let PO = document.querySelector('input#PO')
+    if (FO.value == '' || DE.value == '' || CO.value == '' || IN.value == '' || SA.value == '' || CA.value == '' || PO.value == '') {
+        alert('Algum campo está vazio')
+    } else {
+        FO = Number(FO.value)
+        DE = Number(DE.value)
+        CO = Number(CO.value)
+        IN = Number(IN.value)
+        SA = Number(SA.value)
+        CA = Number(CA.value)
+        let lista = [FO, DE, CO, IN, SA, CA]
+        for (let c = 0; c < 6; c++){
+            if (isNaN(lista[c]) == true) {
+                console.log('Numero')
+                break
+            } else {
+                console.log('Normal')
+                if (c == 5) {
+                    apr()
+                }
             }
         }
     }
-    function apr(){
-        body.innerHTML = ``
-    }
+        
+function apr(){
+    body.innerHTML = ``
+}
 }
